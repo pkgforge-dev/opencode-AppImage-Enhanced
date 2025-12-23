67248de4b7f76bb9ec02bbba2bd437013ab1e28d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-23 08:49:17 -0400

    try to support `aarch64`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,6 +13,9 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
+if [ "$ARCH" = 'aarch64' ]; then
+	PRE_BUILD_CMDS="sed -i -e 's|amd64|arm64|' ./PKGBUILD"
+fi
 make-aur-package opencode-desktop-bin
 
 # If the application needs to be manually built that has to be done down here
