41b31c20c3f5218a3d78c7de9d5ea98071473ebe
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-23 11:42:50 -0400

    work around bun nonsense

--- make-appimage.sh
+++ make-appimage.sh
@@ -16,7 +16,20 @@ quick-sharun \
 	/usr/bin/OpenCode \
 	/usr/bin/opencode-cli
 
-# Additional changes can be done in between here
+# bun makes binaries that self extract and read /proc/self/exe
+# they are also very delicate and get broken by strip
+kek=.$(tr -dc 'A-Za-z0-9_=-' < /dev/urandom | head -c 10)
+rm -f ./AppDir/bin/opencode-cli         ./AppDir/shared/bin/opencode-cli
+cp -v /usr/bin/opencode-cli             ./AppDir/bin/opencode-cli.wrapped
+patchelf --set-interpreter /tmp/"$kek"  ./AppDir/bin/opencode-cli.wrapped
+patchelf --set-rpath '$ORIGIN/../lib'   ./AppDir/bin/opencode-cli.wrapped
+
+cat <<EOF > ./AppDir/bin/opencode-cli
+#!/bin/sh
+cp -f "\$APPDIR"/shared/lib/ld-linux*.so* /tmp/"$kek"
+exec "\$APPDIR"/bin/opencode-cli.wrapped "\$@"
+EOF
+chmod +x ./AppDir/bin/opencode-cli*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
