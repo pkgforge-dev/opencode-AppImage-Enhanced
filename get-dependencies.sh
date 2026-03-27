913bb1450cf85f364a7dca8bc03b81c677a36f05
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-27 05:29:56 -0400

    add missing lib

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm patchelf libnss_nis nss-mdns
+pacman -Syu --noconfirm patchelf libnss_nis nss-mdns nss
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
