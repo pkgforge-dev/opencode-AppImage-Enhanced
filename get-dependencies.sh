452f13ffb13024eef611b192c945bd447b624590
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-27 05:12:01 -0400

    get latest electron deb

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,31 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm patchelf gst-plugins-good gst-plugins-bad libnss_nis nss-mdns
+pacman -Syu --noconfirm patchelf libnss_nis nss-mdns
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
-make-aur-package opencode-desktop-bin
+# make-aur-package
 
-# If the application needs to be manually built that has to be done down here
+echo "Getting binary..."
+echo "---------------------------------------------------------------"
+case "$ARCH" in
+	x86_64)  farch=amd64;;
+	aarch64) farch=arm64;;
+esac
+link=https://github.com/anomalyco/opencode/releases/latest/download/opencode-electron-linux-$farch.deb
+if ! wget --retry-connrefused --tries=30 "$link" -O /tmp/temp.deb 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
+ar xvf /tmp/temp.deb
+tar -xvf ./data.tar.xz
+rm -f ./*.xz /tmp/temp.deb
+
+mkdir -p ./AppDir/bin
+cp -rv ./opt/OpenCode/* ./AppDir/bin
+cp -v ./usr/share/applications/@*.desktop ./AppDir
+cp -v ./usr/share/icons/hicolor/150x150/apps/@*.png ./AppDir
