f16f2b7c54849d0faf21ade86bcd708d97f97d14
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-06-23 09:41:17 +0200

    Add updated `StartupWMClass`

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,6 +9,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/anomalyco/opencode/refs/heads/dev/packages/desktop/icons/prod/128x128.png
 export DEPLOY_OPENGL=1
+export STARTUPWMCLASS=ai.opencode.desktop
 
 # Deploy dependencies
 quick-sharun \
